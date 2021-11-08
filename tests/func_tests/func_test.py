6be330842a2e44bd746c8e8cb4cecad5464f9cec
Shehan Atukorala <shehanatuk@gmail.com>
2021-11-07 21:52:55 -0500

    added functional test to /tests/func_tests/func_test.py

--- tests/func_tests/func_test.py
+++ tests/func_tests/func_test.py
@@ -17,6 +17,13 @@ class FunctionalTestCase(TestCase):
         self.assertIn('Blog', self.browser.page_source)
         # assert True == True 
 
+    @pytest.mark.django_db 
+    def test_actions_dropdown_button_works(self):
+        self.browser.get('http://localhost:8000')
+        self.browser.find_element_by_class_name("dropdown-toggle").click()
+        # FIND WAY TO CHECK IF 'All Articles' <a/> IS VISIBLE
+        self.assertIn("All Articles", self.browser.page_source)
+        self.browser.find_element_by_xpath("//a[contains(text(), 'All Articles')]")
     
     def tearDown(self):
         self.browser.quit()
