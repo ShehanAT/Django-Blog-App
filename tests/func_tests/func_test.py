e400c5843ac8bec84e59afd5d950cff961b51858
Shehan Atukorala <shehanatuk@gmail.com>
2021-11-08 20:51:19 -0500

    added functional test to tests/func_tests/func_test.py

--- tests/func_tests/func_test.py
+++ tests/func_tests/func_test.py
@@ -23,7 +23,26 @@ class FunctionalTestCase(TestCase):
         self.browser.find_element_by_class_name("dropdown-toggle").click()
         # FIND WAY TO CHECK IF 'All Articles' <a/> IS VISIBLE
         self.assertIn("All Articles", self.browser.page_source)
-        self.browser.find_element_by_xpath("//a[contains(text(), 'All Articles')]")
-    
+        all_articles_btn = self.browser.find_element_by_xpath("//a[contains(text(), 'All Articles')]")
+        if all_articles_btn.is_displayed():
+            assert True 
+        else: 
+            pytest.fail() 
+
+    @pytest.mark.django_db 
+    def test_post_link_works(self):
+        self.browser.get('http://localhost:8000')
+        self.browser.find_element_by_class_name("dropdown-toggle").click()
+        # FIND WAY TO CHECK IF 'All Articles' <a/> IS VISIBLE
+        self.assertIn("All Articles", self.browser.page_source)
+        old_url = self.browser.current_url
+        first_post_link = self.browser.find_element_by_xpath("//a[contains(@href, '/post/2')]").click()
+        # all_articles_btn = self.browser.find_element_by_xpath("//a[contains(text(), 'All Articles')]")
+        new_url = self.browser.current_url
+        if old_url != new_url:
+            assert True 
+        else:
+            pytest.fail()
+
     def tearDown(self):
         self.browser.quit()
